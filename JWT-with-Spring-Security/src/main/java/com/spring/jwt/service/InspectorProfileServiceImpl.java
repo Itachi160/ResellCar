c1900c7814ebe6b71913bf17ffd10d65146f436b
Ashutosh7796 <ashutoshshedgeas87@gmail.com>
2024-04-10 19:26:40 +0530

    ADDED: Patch api for InspectorProfile

--- JWT-with-Spring-Security/src/main/java/com/spring/jwt/service/InspectorProfileServiceImpl.java
+++ JWT-with-Spring-Security/src/main/java/com/spring/jwt/service/InspectorProfileServiceImpl.java
@@ -3,8 +3,10 @@ package com.spring.jwt.service;
 import com.spring.jwt.Interfaces.InspectorProfileService;
 import com.spring.jwt.dto.InspectorProfileDto;
 import com.spring.jwt.entity.InspectorProfile;
+import com.spring.jwt.entity.User;
 import com.spring.jwt.exception.UserNotFoundExceptions;
 import com.spring.jwt.repository.InspectorProfileRepo;
+import com.spring.jwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,8 @@ import org.springframework.stereotype.Service;
 public class InspectorProfileServiceImpl implements InspectorProfileService {
 
     private final InspectorProfileRepo inspectorProfileRepo;
+
+    private final UserRepository userRepository;
 
     @Override
     public String updateProfile(InspectorProfileDto inspectorProfileDto, Integer InspectorProfileId) {
@@ -32,6 +36,18 @@ public class InspectorProfileServiceImpl implements InspectorProfileService {
             profile.setLastName(inspectorProfileDto.getLastName());
         }
         inspectorProfileRepo.save(profile);
+
+        User user = profile.getUser();
+        if (user != null) {
+            if (inspectorProfileDto.getMobileNo() != null) {
+                user.setMobileNo(inspectorProfileDto.getMobileNo());
+            }
+            if (inspectorProfileDto.getEmail() != null) {
+                user.setEmail(inspectorProfileDto.getEmail());
+            }
+            userRepository.save(user);
+        }
+
         return "Profile fields updated successfully";
     }
 }
